0ea8b134941a29d70737d448d0ad52e8eced0365
Metkom Lab <32222885+Metkom@users.noreply.github.com>
2017-10-21 08:28:04 +0700

    Add files via upload

---
+++ p1cb.cpp
@@ -0,0 +1,27 @@
+/* Modul Praktikum GP2103 Metode Komputasi, 
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Hardianto Rizky Prabusetyo dan Mohammad Heriyanto
+
+Website: https://osf.io/5tfvq/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi
+DOI: 10.17605/OSF.IO/5TFVQ
+@ Oktober 2017
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	int x,y,a,b,c,d;
+	x=5;
+	y=2;
+	a=x+y; c=x%y;
+	b=x-y; d=x*y;
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+	cout<<"c = "<<c<<endl;
+	cout<<"d = "<<d<<endl;
+}
+
+
+
