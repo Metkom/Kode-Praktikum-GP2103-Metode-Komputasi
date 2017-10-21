d3b7bf31956d177f2d77800c21124d9485afc1de
Metkom Lab <32222885+Metkom@users.noreply.github.com>
2017-10-21 08:31:45 +0700

    Add files via upload

---
+++ p1ob.m
@@ -0,0 +1,17 @@
+%Modul Praktikum GP2103 Metode Komputasi, 
+%Program Studi Teknik Geofisika, Universitas Pertamina
+%Oleh: Hardianto Rizky Prabusetyo dan Mohammad Heriyanto
+
+%Website: https://osf.io/5tfvq/
+%Link Kode: Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi
+%DOI: 10.17605/OSF.IO/5TFVQ
+%@ Oktober 2017
+
+a=1;
+b=5;
+c=6;
+x1=((-b+sqrt(b^2-4*a*c))/2*a)
+x2=((-b-sqrt(b^2-4*a*c))/2*a)
+
+
+
